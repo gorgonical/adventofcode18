5e734d01bc8f57f2f7477d03c7dc3435c3c451b1
Nick Gordon <nicholas.gordon@operamail.com>
2018-12-02 02:18:56 -0500

    Added code for days one and two.

---
+++ day-2.rkt
@@ -0,0 +1,324 @@
+;; BSD 2-Clause License
+
+;; Copyright (c) 2018, Nick Gordon
+;; All rights reserved.
+
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are met:
+
+;; * Redistributions of source code must retain the above copyright notice, this
+;;   list of conditions and the following disclaimer.
+
+;; * Redistributions in binary form must reproduce the above copyright notice,
+;;   this list of conditions and the following disclaimer in the documentation
+;;   and/or other materials provided with the distribution.
+
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+;; FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+;; SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+;; CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+;; OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+;; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+(define input (list "auxwcbzrmdvpsjfgkrthnkioqm"
+"auxwcbzrmdvpsjfgbltonyijqe"
+"auxwcbzrmdfpsefgklthnoioqe"
+"auxwcbzrmdvpsjfgkluhnjisqe"
+"auxwcbzrmdvesjfgdzthnyioqe"
+"auxwcbzrmdvhsjfgklthnmijqe"
+"auxwcbzridvpsjfgkltxeyioqe"
+"ayxwcbzrgdvpsjfgklthiyioqe"
+"ajxwcbzrmdvpsjfgklkhnyiode"
+"auxwcbcrmdvpsjfqelthnyioqe"
+"auxwcbzrmsvpsjsgklthnyiope"
+"auxwcbzrmqvpsjzgklghnyioqe"
+"auxwcbzrmdvpsjtqklthxyioqe"
+"auxwcbzrmdopsjfdklthncioqe"
+"auxwcbzrmdvpsjfgkltmhyfoqe"
+"aixwcbzrmdvpsjfgllthdyeoqe"
+"vuxicbzrmdepsjfgklthnyioqe"
+"auxwcbbxmdkpsjfgklthnyioqe"
+"auxwcbzrgdvpsofaklthnyioqe"
+"auxycbzrmdvpsjfgklthnyuose"
+"aujwcbzrmdvprjfgkltcnyioqe"
+"auxwgbzrmdvpsjfgyzthnyioqe"
+"auxwcbzrmavpsjfgkltsnyiome"
+"auxwcbgrmdvpsjfgkdthnrioqe"
+"kuxwcbzrmdvpsyfgklthnyioue"
+"auxwcbzomdvpjdfgklthnyioqe"
+"auxwcbzrmdppsjfgklthvyifqe"
+"aunwubzrmdvpsjrgklthnyioqe"
+"auxwcbzrmoipsjfgklbhnyioqe"
+"auxwdbzrmdvpsjfgmlthnyioce"
+"auxwcbzjmsvpsjfiklthnyioqe"
+"auxwcbzrmwcpsjfcklthnyioqe"
+"auxwcbzfmdvprjfhklthnyioqe"
+"auxdcbzrgdvpsjfgklthnyxoqe"
+"wuxwbbzrmdvpsjfgklthnyiote"
+"auowcbjrmdvpsjfgklthnyfoqe"
+"auxwsbzrmdvpsjfglltcnyioqe"
+"quxwcbzrmdvpkjfgklthnyioqt"
+"vuxwcbzrudvpsjfgklthnyioqi"
+"puxwcbzrmdvgsjfgklthncioqe"
+"luxdcbzrmdvpsjfgkothnyioqe"
+"auxwcbzrmdvpsjfyklthfhioqe"
+"auxwcbqrmdvpsjfgkldhnyiote"
+"quxwcbzrmlvpsjfgklthnyioqi"
+"auxwcbzgmdvpsjfoklthnyiuqe"
+"auxwcbzrmdvpsbfgkltdjyioqe"
+"auxwcbzsmdrpsjfgklthpyioqe"
+"auxwcbzrmfvpsjfwklthnyiote"
+"auxbkpzrmdvpsjfgklthnyioqe"
+"auxwcbzrddvpsjfsklthnyroqe"
+"abxwcbzrmdvpsjfgkltdnyivqe"
+"awxwcbzrmvvpsjfgklthngioqe"
+"auxwcbzrmkvgsjfgkltcnyioqe"
+"auxwcbammdvpsjfgklthpyioqe"
+"auxwcbhrmdvpsjfgtlthnuioqe"
+"auxwcpzrmdvpbjogklthnyioqe"
+"auxwcbzrmdvpslfgklbhkyioqe"
+"auxwcbsrmdvpjjfgkldhnyioqe"
+"auxwcbzrmdqpsjfgauthnyioqe"
+"ydxwcbxrmdvpsjfgklthnyioqe"
+"auxwcbzrmdvpejfgklthnyyofe"
+"auxwchzrmxvpsjfgklthnyioqh"
+"auxwcbzrtdvpsjfgklxhnzioqe"
+"auxwcbyrmdvpsnfgklnhnyioqe"
+"auxwcbzrcdvpsjugklihnyioqe"
+"auxwcbzrddvpsjfgklhhnyiaqe"
+"aumwtbzrmdvpsjfgklthnyitqe"
+"auxucbzrmdvpsjfgklthwfioqe"
+"auxwcbzrmdvpzmfgkllhnyioqe"
+"auxwcbzrmdvpsjhgklthntiome"
+"buxwzbzrmdvpszfgklthnyioqe"
+"ouxwcbzsgdvpsjfgklthnyioqe"
+"auxwcbzrmdvpsjfskltgnyioqz"
+"auxwcbbrmdvpsjftklthnyioqu"
+"quxocbzrmdvpsjfgklthfyioqe"
+"acxwcbzrmdvpsjfgklfhnrioqe"
+"auxwcbzrmdnpsjfrkjthnyioqe"
+"wuxwybzrmdwpsjfgklthnyioqe"
+"auxwgbxrmdvpsjfghlthnyioqe"
+"atxwcbzrmdvnsjfgklthnyjoqe"
+"acxwcbzmmdvpsjfbklthnyioqe"
+"auxhcbzrmdvbsjbgklthnyioqe"
+"auxwlbzrfdvpsjfgxlthnyioqe"
+"auxwmbzrmdfpsjqgklthnyioqe"
+"auxwcbzrmdvpsgfgklahnyigqe"
+"auxwgbzrmdvpsjfgzldhnyioqe"
+"auxwcbzrmdvpydfgklthnyiohe"
+"auxwxbzrmdvpsjfsklchnyioqe"
+"auxqcbzrmdvpsjfgqlthnyiwqe"
+"auxwcozrmdvssbfgklthnyioqe"
+"auxvcczrmdvpsufgklthnyioqe"
+"auxwcbzrudvpsjfgklyhnyioxe"
+"aulwcbzrmdvpsjqgknthnyioqe"
+"auukcbzrmdvpsjfgklthtyioqe"
+"auxwcszimdvpsjfgklthnyigqe"
+"juxwcbzrbdvpsjfgklthnyboqe"
+"auxwcbzrmdvpjofgklthnyioqj"
+"auxwcbzrmdvpsjfgplfhnyione"
+"auxwcbzrmdhpsjfgkltknyeoqe"
+"luxwcqzrmdvpsjfgklthnbioqe"
+"uuxwcbzrmdvpsjfgkithnyiiqe"
+"auxwcbzrmdvpdjfgkrthnyeoqe"
+"auuwcbnrmdvpsjfgklthnjioqe"
+"auxwcnzrmdvpsjvgklthnyooqe"
+"auxwcbzcmdvpsjfcklthnyiose"
+"auxwcbzrldfpsjfgklthjyioqe"
+"auxwcizrmdvpsjfjklthnymoqe"
+"auxwcbtrmdvpsjfgtlphnyioqe"
+"amxwcbzrmdvksjfgklthnyiove"
+"auxwcbzrmdvpszfgkpthnyiuqe"
+"auxwcbzrmdvxdjfgkltqnyioqe"
+"auxwcbzrudvpsjfgklthnymiqe"
+"auxwcbirmdvfsjfgklmhnyioqe"
+"auwwcbzrndvprjfgklthnyioqe"
+"auxwcbormdgpsjfgklbhnyioqe"
+"auxwabzrmdupsjfgklthnyioqt"
+"auxvcbzrmdvpsjfgkltrmyioqe"
+"auxwcbzrmddpsjfsklthnyizqe"
+"auxwcczrmuvpyjfgklthnyioqe"
+"auxwcczrmdvpsnfgkpthnyioqe"
+"auxkcbzrmdvpsjfhklihnyioqe"
+"auxwcbzrmdvpsjfgklthnkijje"
+"auxwcbzcmdvpsjpgkldhnyioqe"
+"auxwcnzrudvpstfgklthnyioqe"
+"xuxwcbzrgdvusjfgklthnyioqe"
+"aaxwcbzrmdvpsjvgklthnyidqe"
+"auxwcbztmdvpsjfgklthnyhqqe"
+"auxwcbzrmfvpsjfgklthnyilfe"
+"auxwcbzrmdvksjfgklthjyioqq"
+"auxwcbzrmdzksjfgktthnyioqe"
+"auxwcbzrmfvpszfgklohnyioqe"
+"auxwckzamdvpsjfgklthnyioqs"
+"auxwcmzrhdvpsjfaklthnyioqe"
+"fuxwcbzrmdapsjfgklrhnyioqe"
+"avxwxbzrmdvpsjfgklthniioqe"
+"auxwubzrmevpsjfgkltpnyioqe"
+"fuxwcbzrgdvpsjfgklhhnyioqe"
+"duxwwbdrmdvpsjfgklthnyioqe"
+"audwcbzrmdvpnjcgklthnyioqe"
+"auxtcbzrmdvpsjmgklthnyyoqe"
+"aucwcbwrmdepsjfgklthnyioqe"
+"auxwcbzrudvpsjfpklthnyiose"
+"auxwcbzridvpsjfsklthxyioqe"
+"auxtcbzrmdvpscfgklyhnyioqe"
+"auxwcbzrmdvppjfgklthnyivee"
+"auxwdbzrmuvpskfgklthnyioqe"
+"auxwubzrmdvosjfgklthnyiope"
+"auxwcbzrmhnpsjfgklthnyimqe"
+"auxwcbzrmdqpwjfgkltpnyioqe"
+"auxwcbormdvpsjljklthnyioqe"
+"auxwcbzrmdjpsjfgkltjpyioqe"
+"auxwcbzrmdvpszfgklthkyizqe"
+"auxwcbzighvpsjfgklthnyioqe"
+"auxwcbzrmdlpsjfgcythnyioqe"
+"auxwcbzumdvpsjflklthnyimqe"
+"pdxwcbzrmdvpsjfgklthnyihqe"
+"auxwcbzrsdvpsjfgklhhvyioqe"
+"auxwcfzamdvpsjfgkmthnyioqe"
+"aexwcdzrmdvpsjogklthnyioqe"
+"auxxcbkrmavpsjfgklthnyioqe"
+"auxwcbzredvssjfgklthryioqe"
+"aupwqbzrmdvpsjfgklthnyioqc"
+"auxwcbzrmdvpkcagklthnyioqe"
+"auxwcbzrmdvwsbfgklthnlioqe"
+"aunwcbzxmdvhsjfgklthnyioqe"
+"auxwcbzrhddpsjfgklthnnioqe"
+"ouxwcbzrmdvtsifgklthnyioqe"
+"auxwcbzrmdqpsjfgklthnyfoqp"
+"auxwrbzrhdvpsjfgolthnyioqe"
+"auxwcbcqmdvpsjugklthnyioqe"
+"auxwcbzrqdvpsjhgklthnjioqe"
+"auxmcbzrmdvpsjfgmlthnyjoqe"
+"auxwcbzrmdvpsjfgzlthnycoqv"
+"auswcbzrmdvpsffgslthnyioqe"
+"auxwcbzrfdvpsjfrmlthnyioqe"
+"auxwcbzrmdvpsjngzlthnxioqe"
+"auxwcbzrmdvpsjfuqlthnyiyqe"
+"auxwzbzrrdvosjfgklthnyioqe"
+"auxwcbzdmdvpsjfikxthnyioqe"
+"guxwcbzrmdvpsjfgmlthnytoqe"
+"auxwcbzrmdvpspfgkytenyioqe"
+"auxvcbzrldvpsjfgklthnyhoqe"
+"auxwcbzrmavpckfgklthnyioqe"
+"autwcbzrmdvpsafgklthnyirqe"
+"auxwcbzrxuvpsjfgklthmyioqe"
+"auxwcbarmdppsjfgklthnywoqe"
+"anxvcbzrmdvpsjfgklthnyijqe"
+"auxwcbwrmdapsjngklthnyioqe"
+"abxwcbzrmdvpsjugkltgnyioqe"
+"auxwcbtrmdvpsjfgkltunyioue"
+"aujwcbzrmovpsjfgklthryioqe"
+"auxwcbzrydvpsjfgklthndikqe"
+"auxwcbzrmdvpsjfgklmrnyioqo"
+"auxwcbzrddvpsjfggithnyioqe"
+"auxwcbzrmdvpfjfaklthlyioqe"
+"fuxtcbzrmdvpsjfgklwhnyioqe"
+"tuxwcbzrjdvpsjfgjlthnyioqe"
+"auxwcbzrmdppsofgklthnyfoqe"
+"auxvclzamdvpsjfgklthnyioqe"
+"auxwcbzrmdvpsjfdklhhnzioqe"
+"auxwcbzrmsvpsvdgklthnyioqe"
+"arxfcbzrmdvpsvfgklthnyioqe"
+"auxzcbzrmdvpsjfgklthnhioqj"
+"auxwcbzrrdvpsjfgpltunyioqe"
+"auxuibzrmdvpwjfgklthnyioqe"
+"auxwcbzrwdqpsjfgklthnyooqe"
+"aujwcbzrmdvpsjvgklthxyioqe"
+"abxwcbzrmfvpsjfgklthnyxoqe"
+"aurwcbzrmdvpshfgklthnyhoqe"
+"auxwcbzjmdvpsjfgknthnycoqe"
+"auxwcbzrmdvpsjfgklmhxwioqe"
+"auxwcbzrmfvpsjfgklthnyiorq"
+"auxwcbormdvpsjfgklwhnlioqe"
+"auxwctzrmdvpsjfgklcknyioqe"
+"awxwcbzrmdvpsjfgvlthnyiome"
+"auxwcbzrmdvpsjfjklthnyixje"
+"auxwcsxrmdvpsjfgkltsnyioqe"
+"auxbmbzrmdvpsjfgklthnyioce"
+"auxwcbzrmdvpsjfukzthnytoqe"
+"aixwcbzrmdvpsjfgllthdyioqe"
+"auxwcbzrmdypsjfgklthnlioqy"
+"auxccbzrmdvpsjfgkltrnnioqe"
+"auxwcznrmdvpsjfgklthnykoqe"
+"auxwmqzrmdvpsjfgilthnyioqe"
+"auxwcbzrmdvpdyfgolthnyioqe"
+"auxwcbzrmdvpsjfgkmohnqioqe"
+"auxwcfzrmzvpsjfoklthnyioqe"
+"auxwjyzrmdvpsjfgulthnyioqe"
+"auxwcgzredvpsjfgkxthnyioqe"
+"wuxwcbtrmdvpsjfgklthnyiofe"
+"auxwcbzrmdopsgfgklthncioqe"
+"auxmcbzjmdvpsjfgklbhnyioqe"
+"auxwlbzrmdvpsjffklthgyioqe"
+"auxwcbzrmrvpsjfgqlthtyioqe"
+"kuxwhbzrmdvpsjfgklthgyioqe"
+"auxwcozrmdgpsjfgklthnydoqe"
+"auxwdbzrmdvpdjfgklthgyioqe"
+"auxwqbzrmdapsvfgklthnyioqe"
+"auqwcbzridvjsjfgklthnyioqe"
+"auxwckzrmdvpsjfoklthnyuoqe"
+"auxwcbzvmdvpsjfgklghnyiome"
+"auxtcbzrmdvpsjqgktthnyioqe"
+"auxwcbzrmdvesjfgkljhnnioqe"
+"auxwcbzrmpvpsqfgklthnqioqe"
+"auxwcbzrmdcpsqfgklthnzioqe"
+"yuxwcbzrmdvpsjggklthnlioqe"
+"auxwcbzradvpsjftklthoyioqe"
+"auxwcbzrmdvjujfgklmhnyioqe"
+"auxwcbzrmdvpsrfgklpinyioqe"
+"auxwobzrvqvpsjfgklthnyioqe"))
+
+(require racket/function)
+
+;; This solves part 1
+(letrec ([getcounts (lambda (str)
+                   (letrec ([chars (string->list str)]
+                            [charfreqs (map (lambda (char)
+                                              (count ((curry char=?) char) chars))
+                                            chars)]
+                            [hastwo (positive? (count ((curry =) 2) charfreqs))]
+                            [hasthree (positive? (count ((curry =) 3) charfreqs))])
+                     (list hastwo hasthree)))]
+      [allstrs (map getcounts input)]
+      [sumtwos (foldl (lambda (lst acc) (if (first lst) (+ acc 1) acc)) 0 allstrs)]
+      [sumthrees (foldl (lambda (lst acc) (if (second lst) (+ acc 1) acc)) 0 allstrs)]
+      )
+  (* sumtwos sumthrees))
+
+;; This solves part 2
+(require levenshtein)
+
+(define (get-pos-different-char str1 str2)
+  (letrec ([chars1 (string->list str1)]
+           [chars2 (string->list str2)]
+           [helper (lambda (c1 c2 count)
+                     (if (not (char=? (car c1) (car c2)))
+                         count
+                         (helper (cdr c1) (cdr c2) (+ 1 count))))])
+    (helper chars1 chars2 0)))
+
+(define (delete-at pos lst)
+  (cond [(= 0 pos) (cdr lst)]
+        [else (cons (car lst)
+                    (delete-at (- pos 1) (cdr lst)))]))
+
+(let ([firststr null]
+      [secondstr null])
+  (for-each (lambda (str)
+              (for-each (lambda (otherstr) (if (= 1 (string-levenshtein str otherstr))
+                                               (begin
+                                                 (set! firststr str)
+                                                 (set! secondstr otherstr))
+                                               null))
+                        input))
+            input)
+  (let ([pos (get-pos-different-char firststr secondstr)])
+    (list->string (delete-at pos (string->list firststr)))))
