c4b836affaf17b317ed7f8feb7d3775f4c89b2a3
Nick Gordon <nick.gordon@pitt.edu>
2018-12-22 16:38:14 -0500

    Rewrote solution for day 5 to be much better.

--- day-5.lua
+++ day-5.lua
@@ -1,78 +1,44 @@
+require("nmg")
+
 input = "inputs/day5.txt"
-
-function is_upcase(charcode)
-   return charcode > 64 and charcode < 91
-end
-
-function is_lowcase(charcode)
-   return charcode > 96 and charcode < 123
-end
-
-function do_characters_match(lcharcode, rcharcode)
-   return math.abs(lcharcode - rcharcode) == case_difference
-end
 
 -- I don't know any better way to do this. Lua's built-in string parsing functions
 -- are insufficient to read strings character-by-character.
-function select_characters(lastbyte, thisbyte)
-   return
-      ((is_upcase(thisbyte) and is_lowcase(lastbyte)) or
-       (is_lowcase(thisbyte) and is_upcase(lastbyte)))
-      and
-      do_characters_match(lastbyte, thisbyte)
+function do_characters_match(lastbyte, thisbyte)
+   if nmg.strings.is_upcase(lastbyte) and nmg.strings.to_lowcase(lastbyte) == thisbyte then return true
+   elseif nmg.strings.is_lowcase(lastbyte) and nmg.strings.to_lowcase(lastbyte) == thisbyte then return true
+   else return false end
 end
 
--- We need to chunk the string because the string.byte() function has a stack depth
--- limitation. This number was arbitrarily chosen.
-function chunk_string(str)
-   local increment = 5000
-   local start = 1
-   local strings = {}
-   repeat
-      local bytes = { string.byte(str, start, start+increment) }
-      for index, charcode in ipairs(bytes) do
-         table.insert(strings, charcode)
-      end
-      start = start+increment+1
-   until start > #str
-   return strings
-end
-
--- Has side-effects
-function filter_bytes_table(tbl)
-   repeat
-      local deleted = 0
-      for index, charcode in ipairs(tbl) do
-         local nextcharcode = tbl[index+1] or 0
-         if select_characters(charcode, nextcharcode) then
-            -- Because we are removing characters from the table, the index should not change
-            table.remove(tbl, index)
-            table.remove(tbl, index)
-            deleted = deleted + 1
+-- Improved part 1 using a fold
+function reduce_string(str)
+   local accum = ""
+   for character in nmg.strings.characters(str) do
+      -- Starting case
+      if #accum == 0 then accum = accum .. character
+      else
+         -- If the characters match then delete the last character of the accumulator
+         -- and don't do anything with the current character
+         if do_characters_match(string.byte(accum:sub(#accum, #accum)), character:byte()) then
+            accum = accum:sub(1, #accum-1)
+         else -- Else, append the character to the accumulator
+            accum = accum .. character
          end
       end
-   until deleted == 0
+   end
+   return accum
 end
 
--- Filter the string of all adjacent alternate-case characters
+part_1 = ""
 do
    local line = ""
    for inline in io.lines(input) do
       line = inline
    end
 
-   local tbl = chunk_string(line)
-   local case_difference = 32
-
-   filter_bytes_table(tbl)
-
-   local reduced_string = ""
-   for index, charcode in ipairs(tbl) do
-      reduced_string = reduced_string .. string.char(charcode)
-   end
-
-   part1 = reduced_string
-   print("Part 1: " .. #reduced_string)
+   local accum = reduce_string(line)
+   print("Part 1: " .. #accum)
+   part_1 = accum
 end
 
 do
@@ -81,17 +47,11 @@ do
    letters = { "Aa"; "bB"; "cC"; "dD"; "eE"; "fF"; "gG"; "hH"; "iI"; "jJ"; "kK"; "lL";
                "mM"; "nN"; "oO"; "pP"; "qQ"; "rR"; "sS"; "tT"; "uU"; "vV"; "wW"; "xX";
                "yY"; "zZ"; }
-   local filtered = ""
    for index, pair in ipairs(letters) do
-      local filtered_str = string.gsub(part1, "[" .. pair .. "]", "")
-      local filtered_tbl = chunk_string(filtered_str)
-      filter_bytes_table(filtered_tbl)
-      filtered = ""
-      for index, charcode in ipairs(filtered_tbl) do
-         filtered = filtered .. string.char(charcode)
-      end
-      if bestlen == nil then bestlen, bestpair = #filtered, pair
-      elseif #filtered < bestlen then bestlen, bestpair = #filtered, pair
+      local filtered = string.gsub(part_1, "[" .. pair .. "]", "")
+      local reduced = reduce_string(filtered)
+      if bestlen == nil then bestlen, bestpair = #reduced, pair
+      elseif #reduced < bestlen then bestlen, bestpair = #reduced, pair
       end
    end
    print("Part 2: " .. bestlen)
